0af308ec7a5a9fce55645badeab025a53e01cf0e
Yurka2012 <tsapiy.yurka@gmail.com>
2023-04-08 09:50:35 +0300

    12

--- index.js
+++ index.js
@@ -1,92 +1,169 @@
 const TelegramBot = require('node-telegram-bot-api');
-const token = '6122214810:AAH2HudmeBy4Akoda9RL1xRtSx72pBI04SA';
+const htmlValidator = require('html-validator');
+const { validate } = require('./node_modules/html-validator');
 
-const bot = new TelegramBot(token, { polling: true });
 
-// Создаем объект с категориями тегов и соответствующими тегами
+const botToken = '6122214810:AAH2HudmeBy4Akoda9RL1xRtSx72pBI04SA';
+const bot = new TelegramBot(botToken, { polling: true });
+
 const tags = {
-  'Текст': [
-    { tag: '<p>', desc: 'Определяет абзац текста.' },
-    { tag: '<h1>', desc: 'Определяет заголовок верхнего уровня.' },
-    { tag: '<h2>', desc: 'Определяет заголовок второго уровня.' },
-    { tag: '<h3>', desc: 'Определяет заголовок третьего уровня.' },
-    { tag: '<h4>', desc: 'Определяет заголовок четвертого уровня.' },
-    { tag: '<h5>', desc: 'Определяет заголовок пятого уровня.' },
-    { tag: '<h6>', desc: 'Определяет заголовок шестого уровня.' },
-    { tag: '<br>', desc: 'Вставляет перевод строки (перенос на новую строку).' },
-    { tag: '<hr>', desc: 'Определяет горизонтальную линию.' },
-    { tag: '<strong>', desc: 'Выделяет текст жирным шрифтом.' },
-    { tag: '<em>', desc: 'Выделяет текст курсивом.' },
-    { tag: '<u>', desc: 'Выделяет текст подчеркиванием.' },
-    { tag: '<s>', desc: 'Зачеркивает текст.' },
-    { tag: '<blockquote>', desc: 'Определяет цитату из другого источника.' },
-    { tag: '<cite>', desc: 'Определяет название источника цитаты.' },
-    { tag: '<code>', desc: 'Определяет фрагмент кода.' },
-    { tag: '<pre>', desc: 'Определяет блок предварительно отформатированного текста.' }
+  "html": [
+    { "name": "<div>", "description": "Defines a section of the document" },
+    { "name": "<p>", "description": "Defines a paragraph" },
+    { "name": "<a>", "description": "Defines a hyperlink" },
+    { "name": "<img>", "description": "Defines an image" },
+    { "name": "<table>", "description": "Defines a table" },
+    { "name": "<ul>", "description": "Defines an unordered list" },
+    { "name": "<ol>", "description": "Defines an ordered list" },
+    { "name": "<li>", "description": "Defines a list item" }
   ],
-  'Ссылки': [
-    { tag: '<a>', desc: 'Определяет гиперссылку на другую страницу' },
-    { tag: '<abbr>', desc: 'Определяет аббревиатуру' },
-    { tag: '<address>', desc: 'Определяет контактную информацию автора или владельца документа' },
-    { tag: '<area>', desc: 'Определяет активную область изображения' },
-    { tag: '<article>', desc: 'Определяет независимый контент страницы' },
-    { tag: '<aside>', desc: 'Определяет блок содержимого, относящегося к основному содержимому страницы' },
-    { tag: '<audio>', desc: 'Определяет звуковой контент на странице' },
-    { tag: '<b>', desc: 'Определяет жирное начертание текста' },
-    { tag: '<base>', desc: 'Определяет базовый URL для всех относительных URL на странице' },
-    { tag: '<bdi>', desc: 'Изолирует часть текста, которая может быть форматирована в отличии от остального текста' },
-    { tag: '<bdo>', desc: 'Определяет направление текста' },
-    { tag: '<blockquote>', desc: 'Определяет длинную цитату' },
-    { tag: '<body>', desc: 'Определяет тело документа' },
-    { tag: '<br>', desc: 'Определяет перенос строки' },
-    { tag: '<button>', desc: 'Определяет кнопку' },
-    { tag: '<canvas>', desc: 'Определяет область рисования' },
-    { tag: '<caption>', desc: 'Определяет заголовок таблицы' },
-    { tag: '<cite>', desc: 'Определяет название произведения' },
-    { tag: '<code>', desc: 'Определяет блок кода' },
-    { tag: '<col>', desc: 'Определяет свойства одного или нескольких столбцов таблицы' },
-    { tag: '<colgroup>', desc: 'Определяет группу одного или нескольких столбцов таблицы' },
-    { tag: '<data>', desc: 'Определяет значение данных' },
-    { tag: '<datalist>', desc: 'Определяет список предложений для элемента <input>' },
-    { tag: '<dd>', desc: 'Определяет описание термина в списке определений' },
-    { tag: '<del>', desc: 'Определяет удаленный текст' },
-    { tag: '<details>', desc: 'Определяет дополнительные детали' },
-    { tag: '<dfn>', desc: 'Определяет определение термина' },
-    { tag: '<dialog>', desc: 'Определяет диалоговое окно' },
-    { tag: '<div>', desc: 'Определяет блок на веб-странице' },
-    { tag: '<dl>', desc: 'Определяет список определений' },
-    { tag: '<dt>', desc: 'Определяетопределение термина в списке определений' },
-    { tag: '<dd>', desc: 'Определяет определение термина в списке определений' },
-    { tag: '<sup>', desc: 'Определяет верхний индекс' },
-    { tag: '<sub>', desc: 'Определяет нижний индекс' },
-    { tag: '<em>', desc: 'Выделяет текст как ударение' },
-    { tag: '<strong>', desc: 'Выделяет текст как особенно важный' },
-    { tag: '<q>', desc: 'Определяет короткую цитату' },
-    { tag: '<blockquote>', desc: 'Определяет длинную цитату' },
-    { tag: '<cite>', desc: 'Определяет название произведения, которое цитируется' },
-    { tag: '<abbr>', desc: 'Определяет аббревиатуру' },
-    { tag: '<acronym>', desc: 'Определяет акроним' },
-    { tag: '<address>', desc: 'Определяет контактную информацию автора' },
-    { tag: '<bdo>', desc: 'Определяет направление текста' },
-    { tag: '<caption>', desc: 'Определяет заголовок таблицы'},
-    { tag: '<center>', desc: 'Определяет выравнивание содержимого по центру' },
-    { tag: '<col>', desc: 'Определяет свойства для одного или нескольких столбцов в таблице' },
-    { tag: '<colgroup>', desc: 'Определяет группу одного или нескольких столбцов в таблице' },
-    { tag: '<del>', desc: 'Определяет текст, который был удален из документа' },
-    { tag: '<ins>', desc: 'Определяет текст, который был вставлен в документ' },
-    { tag: '<dfn>', desc: 'Определяет термин, который будет определен в тексте' },
-    { tag: '<kbd>', desc: 'Определяет клавиатурный текст' },
-    { tag: '<pre>', desc: 'Определяет предварительно отформатированный текст' },
-    { tag: '<samp>', desc: 'Определяет вывод программы' },
-    { tag: '<var>', desc: 'Определяет переменную программы' },
-    { tag: '<canvas>', desc: 'Определяет область для рисования с помощью JavaScript' },
-    { tag: '<audio>', desc: 'Определяет звуковой поток' },
-    { tag: '<video>', desc: 'Определяет видеопоток' },
-    {tag: '<source>', desc: 'Определяет источник мультимедиа' },
-    { tag: '<embed>', desc: 'Определяет встроенный объект' }]}
-bot.on('message', (msg) => {
+  "forms": [
+    { "name": "<form>", "description": "Defines an HTML form for user input" },
+    { "name": "<input>", "description": "Defines an input control" },
+    { "name": "<select>", "description": "Defines a drop-down list" }
+  ]
+};
+
+// Function to generate the keyboard with tags
+function generateTagsKeyboard(category) {
+  const tagsArray = tags[category];
+  const keyboard = tagsArray.map(tag => [{ text: `${tag.name}` }]);
+  keyboard.push([{ text: 'Back to categories', callback_data: 'back' }]);
+  return {
+    reply_markup: {
+      inline_keyboard: keyboard
+    }
+  };
+}
+
+// Function to generate the keyboard with categories
+function generateCategoriesKeyboard() {
+  const categories = Object.keys(tags);
+  const keyboard = categories.map(category => [{ text: `${category}`, callback_data: `${category}` }]);
+  return {
+    reply_markup: {
+      inline_keyboard: keyboard
+    }
+  };
+}
+
+// Handle the command /start
+bot.onText(/\/start/, (msg) => {
+  const chatId = msg.chat.id;
+  bot.sendMessage(chatId, "Hello! What tag or category would you like to know about?", generateCategoriesKeyboard());
+});
+
+// Handle callback queries from the inline keyboard
+bot.on('callback_query', (query) => {
+  const chatId = query.message.chat.id;
+  if (query.data === 'back') {
+    bot.sendMessage(chatId, "What tag or category would you like to know about?", generateCategoriesKeyboard());
+  } else if (tags.hasOwnProperty(query.data)) {
+    bot.sendMessage(chatId, `Select a tag from the ${query.data} category:`, generateTagsKeyboard(query.data));
+  } else {
+    const tag = tags[query.data].find(t => t.name === query.message.text);
+    bot.sendMessage(chatId, tag.description);
+  }
+});
+
+// Handle any other text messages
+bot.on('message', async (msg) => {
+  const chatId = msg.chat.id;
+  const message = msg.text;
+  
+  // Check if message is HTML code
+  const isHTML = message.startsWith('<') && message.endsWith('>');
+  
+  if (isHTML) {
+    // Validate HTML code
+    const result = await htmlValidator({
+      data: message,
+      format: 'text'
+    });
+    
+    if (result.length > 0) {
+      // There are errors in the HTML code
+      let errorMessage = 'The following errors were found:\n';
+// Iterate over errors and fix them if possible
+for (const error of result.errors) {
+    if (error.ruleId === "tag-not-closed") {
+      // If the error is about an unclosed tag, try to find the tag and close it
+      const unclosedTag = error.element;
+      const lastOpenTag = stack.pop();
+      if (lastOpenTag && lastOpenTag.tagName === unclosedTag.tagName) {
+        // If the last open tag matches the unclosed tag, close it
+        lastOpenTag.closeTagIndex = unclosedTag.startIndex - 1;
+        fixedCode = replaceTag(fixedCode, lastOpenTag);
+      } else {
+        // If there's no matching open tag, insert a closing tag before the unclosed tag
+        const closingTag = createClosingTag(unclosedTag.tagName, unclosedTag.startIndex);
+        fixedCode = insertClosingTag(fixedCode, closingTag);
+      }
+    } else if (error.ruleId === "tag-not-opened") {
+      // If the error is about a tag not opened, try to find the tag and open it
+      const unopenedTag = error.element;
+      const openingTag = createOpeningTag(unopenedTag.tagName, unopenedTag.startIndex);
+      fixedCode = insertOpeningTag(fixedCode, openingTag);
+      stack.push(openingTag);
+    }
+  }
+  
+  // Send the fixed code back to the user
+  bot.sendMessage(chatId, fixedCode);
+// Handle the command /html
+bot.onText(/\/html/, (msg) => {
     const chatId = msg.chat.id;
-  bot.onText(/\/tags/, (msg) => {
-    bot.sendMessage(msg.chat.id, tags, { parse_mode: 'HTML' });
+    bot.sendMessage(chatId, "Please enter your HTML code:");
   });
-})
+  
+  // Handle the user's HTML input
+  bot.on('message', (msg) => {
+    const chatId = msg.chat.id;
+    const html = msg.text;
+    
+    // Validate the HTML using the W3C Markup Validation Service
+    const https = require('https');
+    const validatorURL = `https://validator.w3.org/nu/?out=json`;
+    const postData = `<!DOCTYPE html><html><head><title>Test</title></head><body>${html}</body></html>`;
+    const options = {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'text/html',
+        'Content-Length': postData.length,
+      },
+    };
+    const req = https.request(validatorURL, options, (res) => {
+      let body = '';
+      res.on('data', (chunk) => {
+        body += chunk;
+      });
+      res.on('end', () => {
+        try {
+          const result = JSON.parse(body).messages;
+          if (result.length === 0) {
+            bot.sendMessage(chatId, "Your HTML is valid!");
+          } else {
+            let response = "Your HTML has the following issues:\n\n";
+            for (const error of result) {
+              response += `${error.type}: ${error.message}\n`;
+              if (error.extract) {
+                response += `${error.extract}\n`;
+              }
+              response += `\n`;
+            }
+            bot.sendMessage(chatId, response);
+          }
+        } catch (e) {
+          console.error(e);
+          bot.sendMessage(chatId, "Sorry, there was an error processing your HTML.");
+        }
+      });
+    });
+    req.on('error', (error) => {
+      console.error(error);
+      bot.sendMessage(chatId, "Sorry, there was an error processing your HTML.");
+    });
+    req.write(postData);
+    req.end();
+  });
+}}})
